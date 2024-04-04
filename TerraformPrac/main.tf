ead84b6513baf405918e56ecb7fa69207b904b45
--global <vikasaroorme111@gmail.com>
2024-04-04 09:09:34 +0530

    added terrform dir

---
+++ TerraformPrac/main.tf
@@ -0,0 +1,25 @@
+terraform { 
+    required_version = "~> value"
+    required_providers {
+       aws = { 
+          source = "value"
+          version = "value"
+        }
+    }
+}   
+      
+provider "aws" {
+    region = ""  
+    access_key = "" 
+    secret_key = "" 
+    }   
+        
+provider "aws" {
+    access_key =  "" 
+    secret_key = ""
+
+        
+}
+
+
+
